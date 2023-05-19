c4ab9a308f809cd3199d6028588de2e436b1a9ee
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-05-19 10:47:31 +0200

    [DOCS] Correzioni effettuate. Esercizio completato.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,55 @@
+// VARIABILE BUTTON
 let button = document.getElementById("button")
 
+// INSERIAMO UN ADDEVENTLISTENER AL BUTTON
 button.addEventListener('click', function(){
-
+    // VARIABILE NOME E COGNOME
     let nome_cognome = document.getElementById("name_utente").value
     console.log(nome_cognome)
     document.getElementById("name").innerHTML = nome_cognome 
     
-    
-    let km = document.getElementById("km").value
+    // VARIABILE KM
+    let km = parseInt(document.getElementById("km").value)
     console.log(km)
+
+    // VARIABILE AGE
+    let age = parseInt(document.getElementById("age").value)
+    console.log(age)
     
+    // VARIABILE TARIFFA
     let tariffa = (km * 0.21).toFixed(2)
     console.log(tariffa)
     document.getElementById("costo_biglietto").innerHTML = tariffa+('€')
 
-
+    // VARIABILE CARROZZA
     let carrozza = Math.floor(Math.random() * 10) + 1;
     document.getElementById("carrozza").innerHTML = carrozza
 
+    // VARIABILE CODICE CP
     let codice_cp = (Math.random() * 10 + 1).toFixed(3); 
     document.getElementById("codice_cp").innerHTML = codice_cp
 
+    if (age === 1) {
+        tariffa = (tariffa * 0.8 ).toFixed(2)
+        console.log(tariffa)
+        document.getElementById("costo_biglietto").innerHTML = tariffa+('€')
+        
+    } else if (age === 2) {
+
+        tariffa = (tariffa * 0.6 ).toFixed(2)
+        console.log(tariffa)
+        document.getElementById("costo_biglietto").innerHTML = tariffa+('€')
+
+        
+    }else if (age === 3) {
+        console.log(tariffa)
+
+        
+        
+    }
+
 
     
-    
-
-    
-
-
 })
 
 
--- readme.md
+++ readme.md
@@ -1,3 +1,19 @@
 PROBLEMA:  Scrivere un programma che definisca il prezzo del biglietto (0.21 per km) del treno in base ai km che l'utente voglia percorrere effettuando anche una scontistica del 20% se l utente e minorenne e del 40% se l'utente ha un età maggiore a 65 anni.
 
+1- Inseriamo una variabile che riprenda il primo bottone
+2- Inseriamo un addEventListener che dovrà contenere le funzioni del bottone al suo click
+3- All' interno del del addlistener andiamo a dichiarare tutte le variabili di cui avremo bisogno:
+    3.1 - Variabile che ci indica nome e cognome
+    3.2 - Variabile che ci indica km da percorrere
+    3.3 - Variabile che ci indica l' età
+    3.4 - Variabile che ci indica della tariffa del biglietto
+4- Inoltre dobbiamo controllare le casistiche in cui si va incontro ad una scontistica:
+    SE: l'utente è minorenne avrà uno sconto del 20%
+    ALTRIMENTI SE: l'utente avrà un eta superiore a 65 anni avrà uno sconto del 40%
+5- Inniettiamo nell' HTML sottoforma di testo le nostre variabili dove richiesto
+6- FINE
+
+
+
+
 
