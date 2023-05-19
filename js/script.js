287434d038178c9672819d0c4c2c36a3bf33505b
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-05-19 02:55:55 +0200

    [DOCS] Struttura e Style dell' esercizio completato.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
 let button = document.getElementById("button")
 
 button.addEventListener('click', function(){
-    let nome_cognome = document.getElementById("name").value
-    console.log(nome_cognome)
 
+    let nome_cognome = document.getElementById("name_utente").value
+    console.log(nome_cognome)
+    document.getElementById("name").innerHTML = nome_cognome 
+    
+    
     let km = document.getElementById("km").value
     console.log(km)
     
-    let tariffa = km * 0.21
+    let tariffa = (km * 0.21).toFixed(2)
     console.log(tariffa)
+    document.getElementById("costo_biglietto").innerHTML = tariffa+('€')
+
+
+    let carrozza = Math.floor(Math.random() * 10) + 1;
+    document.getElementById("carrozza").innerHTML = carrozza
+
+    let codice_cp = (Math.random() * 10 + 1).toFixed(3); 
+    document.getElementById("codice_cp").innerHTML = codice_cp
+
+
+    
+    
+
+    
+
 
 })
+
+
+
+
